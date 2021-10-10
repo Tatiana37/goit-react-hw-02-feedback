c0412eb8e1a02dea846cd0ddc953986602362cf6
Tetiana Blizniukova <tatbliz30@gmail.com>
2021-10-10 20:40:38 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import './App.css';
+import React, { Component } from 'react';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Statistics from './components/Statistics/Statistics';
+import Section from './components/Section/Section';
+import Notification from './components/Notification/Notification';
+import Container from './components/Container/Container';
 
-function App() {
-  return <div className="App">hw-02</div>;
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  render() {
+    const { good } = this.state;
+    const { neutral } = this.state;
+    const { bad } = this.state;
+    const countTotalFeedback = good + neutral + bad;
+    const countPositiveFeedbackPercentage = Math.round(
+      (good / countTotalFeedback) * 100,
+    );
+    const options = Object.keys(this.state);
+    return (
+      <Container>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+          {countTotalFeedback ? (
+            <Section title="Statistics">
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={countTotalFeedback}
+                percentage={countPositiveFeedbackPercentage}
+              ></Statistics>
+            </Section>
+          ) : (
+            <Notification message="No feedback given" />
+          )}
+        </Section>
+      </Container>
+    );
+  }
 }
-
 export default App;
